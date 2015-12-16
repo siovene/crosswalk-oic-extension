3cf3a949ec22e544b4cd32642be2a51346ce00ef
Salvatore Iovene <salvatore.iovene@intel.com>
2015-12-16 10:46:02 +0200

    Add mjson

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
 ARCH="x86"
+
 NDK="$HOME/code/android-ndk-r10e"
 SYSROOT="$NDK/platforms/android-21/arch-x86"
-CC="$NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/i686-linux-android-gcc --sysroot=$SYSROOT"
-EXTENSION_LIB="libcrosswalk-oic.so"
 
+CC="$NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/i686-linux-android-gcc --sysroot=$SYSROOT"
+CFLAGS="-O0 -g -fPIC -rdynamic -shared -m32 -Wl,--hash-style=sysv"
+
+EXTENSION_LIB="libcrosswalk-oic.so"
+SOURCES="json/mjson.c iotivity/iotivity_api.c iotivity/iotivity_extension.c"
+
+# Prepare folders
 mkdir -p out/$ARCH
-python iotivity/generate_api.py iotivity/iotivity_api.js kSource_iotivity_api iotivity/iotivity_api.c
-$CC -O0 -g -fPIC -rdynamic -shared -m32 -Wl,--hash-style=sysv -o out/$ARCH/$EXTENSION_LIB iotivity/iotivity_api.c iotivity/iotivity_extension.c
+
+# Generage API
+python \
+    iotivity/generate_api.py \
+    iotivity/iotivity_api.js \
+    kSource_iotivity_api \
+    iotivity/iotivity_api.c
+
+# Build
+$CC \
+   $CFLAGS \
+   -o out/$ARCH/$EXTENSION_LIB \
+   $SOURCES
